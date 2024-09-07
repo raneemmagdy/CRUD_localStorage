bc3070d3b1b3f5d67f9b1680ba0e5736fe5880cc
raneemmagdy <raneemmagdy2002@gmail.com>
2024-09-07 18:33:01 +0300

    Validation

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@ var productDescriptionInput=document.getElementById("productDescriptionInput");
 var searchInput=document.getElementById("searchInput")
 var addBtn=document.getElementById("addBtn")
 var updateBtn=document.getElementById("updateBtn")
+var alertName=document.getElementById("alertName")
+var alertPrice=document.getElementById("alertPrice")
+var alertCategory=document.getElementById("alertCategory")
+var alertDescription=document.getElementById("alertDescription")
 var productList=[];
 var indexUpdate=0;
 if(localStorage.getItem('Products')!= null){
@@ -18,14 +22,16 @@ if(localStorage.getItem('Products')!= null){
 
 
 function AddProduct(){
-   var product={
-    productName:productNameInput.value,
-    productPrice:productPriceInput.value,
-    productCategory:productCategoryInput.value,
-    productDescription:productDescriptionInput.value,
-   }
-  productList.push(product);
-  localStorage.setItem('Products',JSON.stringify(productList))
+  if(validationName() && validationPrice()&&validationCategory()&&validationDescription()){
+    var product={
+      productName:productNameInput.value,
+      productPrice:productPriceInput.value,
+      productCategory:productCategoryInput.value,
+      productDescription:productDescriptionInput.value,
+     }
+    productList.push(product);
+    localStorage.setItem('Products',JSON.stringify(productList))
+  }
   clearInput();
   displayData()
   // console.log(productList);
@@ -111,4 +117,78 @@ function updateProduct(){
    clearInput();
    updateBtn.classList.add('d-none')
    addBtn.classList.remove('d-none')
+}
+
+
+function validationName(){
+  var text=productNameInput.value
+  var regexName=/^[A-Z][a-z]{3,10}$/
+
+  if(regexName.test(text)){
+    productNameInput.classList.add('is-valid')
+    productNameInput.classList.remove('is-invalid')
+    alertName.classList.add('d-none')
+    return true
+
+  }else{
+    alertName.classList.remove('d-none')
+    productNameInput.classList.add('is-invalid')
+    productNameInput.classList.remove('is-valid')
+    return false
+  }
+}
+
+
+function validationPrice(){
+  var text=productPriceInput.value
+  var regexName=/^([1-9][0-9]{0,6}|10000000)$/
+
+  if(regexName.test(text)){
+    productPriceInput.classList.add('is-valid')
+    productPriceInput.classList.remove('is-invalid')
+    alertPrice.classList.add('d-none')
+    return true
+
+  }else{
+    alertPrice.classList.remove('d-none')
+    productPriceInput.classList.add('is-invalid')
+    productPriceInput.classList.remove('is-valid')
+    return false
+  }
+}
+
+
+function validationCategory(){
+  var text=productCategoryInput.value
+  var regexName=/^[A-Z][a-z]{3,20}$/
+
+  if(regexName.test(text)){
+    productCategoryInput.classList.add('is-valid')
+    productCategoryInput.classList.remove('is-invalid')
+    alertCategory.classList.add('d-none')
+    return true
+
+  }else{
+    alertCategory.classList.remove('d-none')
+    productCategoryInput.classList.add('is-invalid')
+    productCategoryInput.classList.remove('is-valid')
+    return false
+  }
+}
+function validationDescription(){
+  var text=productDescriptionInput.value
+  var regexName=/^[a-z]{3,100}$/i
+
+  if(regexName.test(text)){
+    productDescriptionInput.classList.add('is-valid')
+    productDescriptionInput.classList.remove('is-invalid')
+    alertDescription.classList.add('d-none')
+    return true
+
+  }else{
+    alertDescription.classList.remove('d-none')
+    productDescriptionInput.classList.add('is-invalid')
+    productDescriptionInput.classList.remove('is-valid')
+    return false
+  }
 }
